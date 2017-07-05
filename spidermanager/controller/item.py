5d106ef03bc7f0d707026b553037be833c6206b3
bingo <bingo@BinGo>
2017-07-05 19:35:26 +0800

    前台展示

--- spidermanager/controller/item.py
+++ spidermanager/controller/item.py
@@ -1,212 +1,53 @@
 # -*- coding: utf-8 -*-
-import json
-
-from flask import render_template
+from flask import render_template, request, redirect, url_for, session
 
 from spidermanager import app
+from spidermanager.setting import SHOW_DATABASE_URI
+import sqlalchemy.exc
+from spidermanager.util.action_result import list2json, obj2json
+from sqlalchemy import (create_engine, MetaData, Table, Column,
+                        String, Float, LargeBinary)
+import json
 
+engine = create_engine(SHOW_DATABASE_URI, convert_unicode=True,pool_recycle=3600)
+@app.route("/item", methods=['GET','POST'])
+def load_dict():
+    dicts = []
+    try:
+        for res in engine.execute("select TBL_ID,MODEL_TBL_DESC,MODEL_TBL_SPD,MODEL_TBL_TYPE,DATA_SOURCE,TOTAL_RECORDS,LAST_UPDATE from dict_spd_data where STATUS in ('A','P')"):
+            tmp_dict = {}
+            tmp_dict['tableId'] = res[0]
+            tmp_dict['tableName'] = res[1]
+            tmp_dict['spdTableName'] = res[2]
+            tmp_dict['tableType'] = res[3]
+            tmp_dict['dataSource'] = res[4]
+            tmp_dict['totalRecords'] = res[5]
+            tmp_dict['lastUpdate'] = res[6]
+            dicts.append(tmp_dict)
+    except sqlalchemy.exc.SQLAlchemyError,e:
+        print e
+    return json.dumps(dicts,ensure_ascii=False)
 
-@app.route("/item", methods=['GET'])
-def item():
-    itemJson = [
-        {
-            "tableId": "10000",
-            "tableName": "豆瓣电影信息",
-            "spdTableName": "douban_video",
-            "tableType": "视频",
-            "dataSource": "豆瓣电影",
-            "totalRecords": "60487",
-            "lastUpdate": "2017/7/3 9:39:10"
-        },
-        {
-            "tableId": "10000",
-            "tableName": "豆瓣电影信息",
-            "spdTableName": "douban_video2",
-            "tableType": "视频",
-            "dataSource": "豆瓣电影",
-            "totalRecords": "60487",
-            "lastUpdate": "2017/7/3 9:39:10"
-        }, {
-            "tableId": "10000",
-            "tableName": "豆瓣电影信息",
-            "spdTableName": "douban_video3",
-            "tableType": "视频",
-            "dataSource": "豆瓣电影",
-            "totalRecords": "60487",
-            "lastUpdate": "2017/7/3 9:39:10"
-        },
-        {
-            "tableId": "10000",
-            "tableName": "豆瓣电影信息",
-            "spdTableName": "douban_video4",
-            "tableType": "视频",
-            "dataSource": "豆瓣电影",
-            "totalRecords": "60487",
-            "lastUpdate": "2017/7/3 9:39:10"
-        }, {
-            "tableId": "10000",
-            "tableName": "豆瓣电影信息",
-            "spdTableName": "douban_video5",
-            "tableType": "视频",
-            "dataSource": "豆瓣电影",
-            "totalRecords": "60487",
-            "lastUpdate": "2017/7/3 9:39:10"
-        },
-        {
-            "tableId": "10000",
-            "tableName": "豆瓣电影信息",
-            "spdTableName": "douban_video6",
-            "tableType": "视频",
-            "dataSource": "豆瓣电影",
-            "totalRecords": "60487",
-            "lastUpdate": "2017/7/3 9:39:10"
-        }, {
-            "tableId": "10000",
-            "tableName": "豆瓣电影信息",
-            "spdTableName": "douban_video7",
-            "tableType": "视频",
-            "dataSource": "豆瓣电影",
-            "totalRecords": "60487",
-            "lastUpdate": "2017/7/3 9:39:10"
-        },
-        {
-            "tableId": "10000",
-            "tableName": "豆瓣电影信息",
-            "spdTableName": "douban_video8",
-            "tableType": "视频",
-            "dataSource": "豆瓣电影",
-            "totalRecords": "60487",
-            "lastUpdate": "2017/7/3 9:39:10"
-        }, {
-            "tableId": "10000",
-            "tableName": "豆瓣电影信息",
-            "spdTableName": "douban_video9",
-            "tableType": "视频",
-            "dataSource": "豆瓣电影",
-            "totalRecords": "60487",
-            "lastUpdate": "2017/7/3 9:39:10"
-        },
-        {
-            "tableId": "10000",
-            "tableName": "豆瓣电影信息",
-            "spdTableName": "douban_video10",
-            "tableType": "视频",
-            "dataSource": "豆瓣电影",
-            "totalRecords": "60487",
-            "lastUpdate": "2017/7/3 9:39:10"
-        }, {
-            "tableId": "10000",
-            "tableName": "豆瓣电影信息",
-            "spdTableName": "douban_video11",
-            "tableType": "视频",
-            "dataSource": "豆瓣电影",
-            "totalRecords": "60487",
-            "lastUpdate": "2017/7/3 9:39:10"
-        },
-        {
-            "tableId": "10000",
-            "tableName": "豆瓣电影信息",
-            "spdTableName": "douban_video12",
-            "tableType": "视频",
-            "dataSource": "豆瓣电影",
-            "totalRecords": "60487",
-            "lastUpdate": "2017/7/3 9:39:10"
-        }, {
-            "tableId": "10000",
-            "tableName": "豆瓣电影信息",
-            "spdTableName": "douban_video13",
-            "tableType": "视频",
-            "dataSource": "豆瓣电影",
-            "totalRecords": "60487",
-            "lastUpdate": "2017/7/3 9:39:10"
-        },
-        {
-            "tableId": "10000",
-            "tableName": "豆瓣电影信息",
-            "spdTableName": "douban_video14",
-            "tableType": "视频",
-            "dataSource": "豆瓣电影",
-            "totalRecords": "60487",
-            "lastUpdate": "2017/7/3 9:39:10"
-        }, {
-            "tableId": "10000",
-            "tableName": "豆瓣电影信息",
-            "spdTableName": "douban_video15",
-            "tableType": "视频",
-            "dataSource": "豆瓣电影",
-            "totalRecords": "60487",
-            "lastUpdate": "2017/7/3 9:39:10"
-        },
-        {
-            "tableId": "10000",
-            "tableName": "豆瓣电影信息",
-            "spdTableName": "douban_video16",
-            "tableType": "视频",
-            "dataSource": "豆瓣电影",
-            "totalRecords": "60487",
-            "lastUpdate": "2017/7/3 9:39:10"
-        }
-    ]
-    return json.dumps(itemJson)
-
-
-@app.route("/tableDetail", methods=['GET'])
-def tableDetail():
-    tableDetail = {
-        "tableDesc": [
-            {
-                "columnName": "XXX",
-                "columnDesc": "XXX"
-            },
-            {
-                "columnName": "YYY",
-                "columnDesc": "YYY"
-            },            {
-                "columnName": "XXX",
-                "columnDesc": "XXX"
-            },
-            {
-                "columnName": "YYY",
-                "columnDesc": "YYY"
-            },            {
-                "columnName": "XXX",
-                "columnDesc": "XXX"
-            },
-            {
-                "columnName": "YYY",
-                "columnDesc": "YYY"
-            },            {
-                "columnName": "XXX",
-                "columnDesc": "XXX"
-            },
-            {
-                "columnName": "YYY",
-                "columnDesc": "YYY"
-            },            {
-                "columnName": "XXX",
-                "columnDesc": "XXX"
-            },
-            {
-                "columnName": "YYY",
-                "columnDesc": "YYY"
-            },            {
-                "columnName": "XXX",
-                "columnDesc": "XXX"
-            },
-            {
-                "columnName": "YYY",
-                "columnDesc": "YYY"
-            },            {
-                "columnName": "XXX",
-                "columnDesc": "XXX"
-            },
-            {
-                "columnName": "YYY",
-                "columnDesc": "YYY"
-            }
-        ],
-        "sampleData": {
-            "XXX": "aaa", "YYY": "bbb"
-        }
-    }
-    return json.dumps(tableDetail)
+@app.route("/tableDetail", methods=['GET','POST'])
+def table_detail():
+    table_name = request.values.get('tableName')
+    table_detail = {}
+    tableDesc = []
+    sampleData = {}
+    col_name = []
+    try:
+        for res in engine.execute("SELECT column_name, comments FROM user_col_comments where table_name = '%s'"%table_name):
+            tmp = {}
+            tmp['columnName']=res[0]
+            tmp['columnDesc']=res[1].decode('gbk').encode('utf8')
+            col_name.append(res[0])
+            tableDesc.append(tmp)
+        sql = "select %s from %s where rownum=1"%(",".join(col_name),table_name)
+        for res in engine.execute(sql):
+            for i in range(len(res)):
+                sampleData[col_name[i]] = res[i]
+        table_detail['tableDesc'] = tableDesc 
+        table_detail['sampleData'] = sampleData 
+    except sqlalchemy.exc.SQLAlchemyError,e:
+        print e 
+    return json.dumps(table_detail,ensure_ascii=False)
