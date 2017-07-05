051a5846cbe00c0ec2cb8a94feb7afa75b1b2f3d
bingo <bingo@BinGo>
2017-07-05 19:57:30 +0800

    fix Chinese problem

--- spidermanager/controller/item.py
+++ spidermanager/controller/item.py
@@ -19,13 +19,13 @@ def load_dict():
     try:
         for res in engine.execute("select TBL_ID,MODEL_TBL_DESC,MODEL_TBL_SPD,MODEL_TBL_TYPE,DATA_SOURCE,TOTAL_RECORDS,TO_CHAR(LAST_UPDATE, 'yyyy-MM-dd HH24:mm:ss') from dict_spd_data where STATUS in ('A','P')"):
             tmp_dict = {}
-            tmp_dict['tableId'] = res[0].decode('gbk').encode('utf8')
+            tmp_dict['tableId'] = res[0]
             tmp_dict['tableName'] = res[1].decode('gbk').encode('utf8')
-            tmp_dict['spdTableName'] = res[2].decode('gbk').encode('utf8')
+            tmp_dict['spdTableName'] = res[2]
             tmp_dict['tableType'] = res[3].decode('gbk').encode('utf8')
             tmp_dict['dataSource'] = res[4].decode('gbk').encode('utf8')
-            tmp_dict['totalRecords'] = res[5].decode('gbk').encode('utf8')
-            tmp_dict['lastUpdate'] = res[6].decode('gbk').encode('utf8')
+            tmp_dict['totalRecords'] = res[5]
+            tmp_dict['lastUpdate'] = res[6]
             dicts.append(tmp_dict)
     except sqlalchemy.exc.SQLAlchemyError,e:
         print e
