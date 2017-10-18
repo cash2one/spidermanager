a997030804707ded39d3a00b432371580ee528d4
hackty <ty@puton.info>
2017-10-18 09:25:56 +0800

    fix bugs of gbk

--- spidermanager/controller/user.py
+++ spidermanager/controller/user.py
@@ -105,7 +105,7 @@ def load():
 
     for user in users:
         try:
-            user.description = user.description.decode('gbk')
+            user.description = user.description.decode('gbk').encode('utf8')
         except Exception,e:
             print e
 
